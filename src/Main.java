72a7e5dc59bc3ce67709e2834155edd4bf710daf
Yuel <beles.dev@gmail.com>
2024-09-16 21:00:06 +0200

    added the week and minute counter!

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,14 @@ public class Main {
 
         int month_year = 12;
         double month_age = age * month_year;
+        double week_age = age * 52.14 ;
+        float minute_age = (float) (age * 365.25 * 24 * 60);
 
 
 
         System.out.println("Din ålder i månader är " + month_age);
-
+        System.out.println("Din ålder i veckor är " + week_age);
+        System.out.println("Din ålder i minuter är " + minute_age);
 
 
 
